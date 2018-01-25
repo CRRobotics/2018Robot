c1e3a9ab91ad74a20352ef2efaa60db147a22a8e
Jack Greenberg <theProgrammerJack@gmail.com>
2018-01-25 18:59:25 -0500

    Updated RobotMap with correct ports. Added ports.md to contain port info.

---
+++ ports.md
@@ -0,0 +1,19 @@
+# Ports
+
+## Drive
+### Left
+* Talon: 0
+* Victors: 1, 2
+
+### Right
+* Talon: 3
+* Victors: 4, 5
+
+## Acquisition
+* Left: 8
+* Right: 9
+
+## Lift
+* Main Talon: 6
+* Follower Talon: 7
+* (These were rather arbitrarily chosen. Either one could be the follower.)
--- src/main/java/org/team639/robot/RobotMap.java
+++ src/main/java/org/team639/robot/RobotMap.java
@@ -1,6 +1,7 @@
 package org.team639.robot;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SPI;
@@ -14,16 +15,27 @@ import org.team639.lib.sensor.distance.MaxSonarEZ4Analog;
 public class RobotMap {
     private static boolean initialized = false;
 
+    // Left drive
     private static TalonSRX leftDrive;
-    private static TalonSRX rightDrive;
+    private static VictorSPX leftFollower1;
+    private static VictorSPX leftFollower2;
 
+    // Right drive
+    private static TalonSRX rightDrive;
+    private static VictorSPX rightFollower1;
+    private static VictorSPX rightFollower2;
+
+    // NAVX
     private static AHRS ahrs;
 
-
+    // Acquisition
     private static TalonSRX leftAcquisition;
     private static TalonSRX rightAcquisition;
-
     private static DigitalInput cubeDetector;
+
+    // Lift
+    private static TalonSRX liftMain;
+    private static TalonSRX liftFollower;
 
     private RobotMap() {}
 
@@ -33,38 +45,108 @@ public class RobotMap {
      */
     public static void init() {
         if (!initialized) {
-            leftDrive = new TalonSRX(1);
-            rightDrive = new TalonSRX(2);
+            // Left drive
+            leftDrive = new TalonSRX(0);
+            leftFollower1 = new VictorSPX(1);
+            leftFollower2 = new VictorSPX(2);
+
+            // Right drive
+            rightDrive = new TalonSRX(3);
+            rightFollower1 = new VictorSPX(4);
+            rightFollower2 = new VictorSPX(5);
+
+            // NAVX
             ahrs = new AHRS(SPI.Port.kMXP);
 
-            leftAcquisition = new TalonSRX(3);
-            rightAcquisition = new TalonSRX(4);
+            // Acquisition
+            leftAcquisition = new TalonSRX(8);
+            rightAcquisition = new TalonSRX(9);
             cubeDetector = new DigitalInput(0);
+
+            // Lift
+            liftMain = new TalonSRX(6);
+            liftFollower = new TalonSRX(7);
 
             initialized = true;
         }
     }
 
+    /**
+     * Returns the left side Talon.
+     * @return The left side Talon.
+     */
     public static TalonSRX getLeftDrive() {
         return leftDrive;
     }
 
+    /**
+     * Returns the first left side Victor.
+     * @return the first left side Victor.
+     */
+    public static VictorSPX getLeftFollower1() {
+        return leftFollower1;
+    }
+
+    /**
+     * Returns the second left side Victor.
+     * @return the second left side Victor.
+     */
+    public static VictorSPX getLeftFollower2() {
+        return leftFollower2;
+    }
+
+    /**
+     * Returns the right side Talon.
+     * @return The right side Talon.
+     */
     public static TalonSRX getRightDrive() {
         return rightDrive;
     }
 
+    /**
+     * Returns the first right side Victor.
+     * @return The first right side Victor.
+     */
+    public static VictorSPX getRightFollower1() {
+        return rightFollower1;
+    }
+
+    /**
+     * Returns the second right side Victor.
+     * @return The second right side Victor.
+     */
+    public static VictorSPX getRightFollower2() {
+        return rightFollower2;
+    }
+
+    /**
+     * Returns the navX gyro.
+     * @return The navX gyro.
+     */
     public static AHRS getAhrs() {
         return ahrs;
     }
 
+    /**
+     * Returns the Talon controlling the left side of the acquisition.
+     * @return The Talon controlling the left side of the acquisition.
+     */
     public static TalonSRX getLeftAcquisition() {
         return leftAcquisition;
     }
 
+    /**
+     * Returns the Talon controlling the right side of the acquisition.
+     * @return The Talon controlling the right side of the acquisition.
+     */
     public static TalonSRX getRightAcquisition() {
         return rightAcquisition;
     }
 
+    /**
+     * Returns the IR cube detector on the acquisition.
+     * @return The IR cube detector on the acquisition.
+     */
     public static DigitalInput getCubeDetector() {
         return cubeDetector;
     }
