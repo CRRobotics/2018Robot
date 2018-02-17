9db76be42029b24a437e9df18422b5bd1db2bc9c
Jack Greenberg <theProgrammerJack@gmail.com>
2018-02-16 21:06:06 -0500

    UNTESTED. More additions to OneCubeScale auto period.

--- src/main/java/org/team639/robot/commands/auto/OneCubeScale.java
+++ src/main/java/org/team639/robot/commands/auto/OneCubeScale.java
@@ -12,15 +12,19 @@ public class OneCubeScale extends CommandGroup {
         MatchData.OwnedSide side = MatchData.getOwnedSide(MatchData.GameFeature.SCALE);
         StartingPosition position = Robot.getStartingPosition();
 
-        addSequential(new AutoDriveForward(10));
+
         if ((side == MatchData.OwnedSide.RIGHT && position == StartingPosition.Right) || (side == MatchData.OwnedSide.LEFT && position == StartingPosition.Left)) {
+            addSequential(new AutoDriveForward(10));
             double relAngle = Math.toDegrees(Math.atan(41.88 / 247.15)) * (position == StartingPosition.Left ? -1 : 1);
             addSequential(new AutoTurnToAngle(90 + relAngle));
             addSequential(new AutoDriveForward(Math.sqrt(Math.pow(41.88, 2) + Math.pow(247.15, 2))));
+            addSequential(new AutoDriveForward(10));
         } else {
-            // TODO: Implement other case.
+            addSequential(new AutoDriveForward(219));
+            int direction = position == StartingPosition.Left ? -1 : 1;
+            addSequential(new AutoTurnToAngle(90 + 90 * direction));
+            addSequential(new AutoDriveForward(80.65));
         }
-        addSequential(new AutoDriveForward(10));
 
         // TODO: Actually place a cube here.
     }
