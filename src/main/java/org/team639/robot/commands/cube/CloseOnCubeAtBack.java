07a1993ad49759697f48897344be46f35c3a5bef
Jack Greenberg <theProgrammerJack@gmail.com>
2018-02-19 09:54:27 -0500

    Added more comments and generated documentation.

--- src/main/java/org/team639/robot/commands/cube/CloseOnCubeAtBack.java
+++ src/main/java/org/team639/robot/commands/cube/CloseOnCubeAtBack.java
@@ -9,6 +9,9 @@ import org.team639.robot.commands.cube.WaitForCubeAtBack;
 
 import static org.team639.robot.Constants.DEFAULT_ACQ_SPEED;
 
+/**
+ * Starts intaking a cube once the back sensor is triggered.
+ */
 public class CloseOnCubeAtBack extends CommandGroup {
     public CloseOnCubeAtBack() {
         addSequential(new LowerAcquisition());
