477cb7356651a6b7b742a69efba0759ddb797ac7
Jack Greenberg <theProgrammerJack@gmail.com>
2018-03-14 17:53:48 -0400

    UNTESTED. Enable with piston floating to work around pnuematics charging problem. Close the piston as soon as auto begins.

--- src/main/java/org/team639/robot/commands/auto/AutoBoilerplate.java
+++ src/main/java/org/team639/robot/commands/auto/AutoBoilerplate.java
@@ -4,12 +4,14 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.PrintCommand;
 import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.team639.robot.commands.cube.CloseAcquisition;
 import org.team639.robot.commands.drive.ZeroYaw;
 import org.team639.robot.commands.lift.ZeroLift;
 
 public class AutoBoilerplate extends CommandGroup {
     public AutoBoilerplate(Command auto, double delay) {
         addSequential(new ZeroYaw());
+        addSequential(new CloseAcquisition());
         addSequential(new PrintCommand("Starting boilerplate -- " + auto.getClass().getName()));
 //        addParallel(new ZeroLift(), 1); FIXME: Put back
         addSequential(new PrintCommand("zeroed lift"));
